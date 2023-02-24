9ead14f811e5dd63dacfa9b2b194a2db2af47358
hasan basri uzun <uzun.hbasri@gmail.com>
2023-02-24 19:11:20 +0300

    Create hbasriuzun.cpp

---
+++ Algorithms_Sources/Algorithm Questions/Algoeague_My_Dog_Ate_My_Homework/hbasriuzun.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+long factorial(const int n)
+{
+    long f = 1;
+    for (int i=1; i<=n; ++i)
+        f *= i;
+    return f;
+}
+
+long find_numbers(int n){
+    
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int a[n];
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    int per = 0;
+    __is_permutation
+    while (per < pow(10,5))
+    {
+        
+    }
+    
+    
+    return 0;
+}
